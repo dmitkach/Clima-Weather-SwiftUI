c8c805dcf3555e5f5a6c0bfb9b211181ad9b9237
Dmitry Tkach <dmitkach@icloud.com>
2024-09-11 22:46:58 +0300

    Initial Commit

---
+++ Clima-Weather-SwiftUI/Clima_Weather_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clima_Weather_SwiftUIApp.swift
+//  Clima-Weather-SwiftUI
+//
+//  Created by Dmitry Tkach on 11.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct Clima_Weather_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
